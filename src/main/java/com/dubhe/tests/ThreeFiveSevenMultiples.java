e0231db7d582c601d7a31916df84f339ec8aaaa8
jorgec <jorgec@meta4.com>
2021-09-21 15:12:55 +0200

    Clean code

--- src/main/java/com/dubhe/tests/ThreeFiveSevenMultiples.java
+++ src/main/java/com/dubhe/tests/ThreeFiveSevenMultiples.java
@@ -2,16 +2,11 @@ package com.dubhe.tests;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Hello world!
  */
 public class ThreeFiveSevenMultiples {
-
-	public ThreeFiveSevenMultiples() {
-	}
 
 	private static int sumMultiplesOf357forloop(int[] numbers) {
 
