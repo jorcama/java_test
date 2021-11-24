cd72ce345ae878892ad2486ebb8e34be03f0e94e
jorgec <jorgec@meta4.com>
2021-11-24 10:07:56 +0100

    OkHttp test

---
+++ src/test/java/com/dubhe/tests/http/OkHttp.java
@@ -0,0 +1,32 @@
+package com.dubhe.tests.http;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import org.junit.Test;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OkHttp {
+
+	@Test
+	public void shouldGenerateAStringToken()
+			throws IOException {
+		
+		String sURL = "https://datos.madrid.es/egob/catalogo/format/fmt.json"; 
+		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		Request request = new Request.Builder().url(sURL)
+				.method("GET", null)
+				.addHeader("Accept", "application/json")
+				.build();
+
+		Response response = client.newCall(request).execute();
+		
+		System.out.println(response.body().string());
+		assertEquals(HttpURLConnection.HTTP_OK, response.code());
+	}
+}
