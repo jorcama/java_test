1ef88629ee601e9a50482a7b63bba5073170ee4b
jorgec <jorgec@meta4.com>
2022-01-19 14:11:29 +0100

    Add CheckSum

--- src/test/java/com/dubhe/tests/FileTest.java
+++ src/test/java/com/dubhe/tests/FileTest.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.assertEquals;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedInputStream;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,8 +21,9 @@ public class FileTest {
 	public void setUp() {
 	}
 
-	private String readFile(File file) throws IOException {
-		
+	private String readFile(File file)
+			throws IOException {
+
 		BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()));
 		StringBuilder fileContent = new StringBuilder();
 		String line = reader.readLine();
@@ -28,23 +32,49 @@ public class FileTest {
 			line = reader.readLine();
 		}
 		reader.close();
-		
+
 		return fileContent.toString();
 	}
-	
+
+	private static long getChecksumCRC32(InputStream stream, int bufferSize)
+			throws IOException {
+		CheckedInputStream checkedInputStream = new CheckedInputStream(stream, new CRC32());
+		byte[] buffer = new byte[bufferSize];
+		while (checkedInputStream.read(buffer, 0, buffer.length) >= 0) {
+		}
+		return checkedInputStream.getChecksum().getValue();
+	}
+
 	@Test
-	public void createTemporalFile() throws IOException {
+	public void createTemporalFile()
+			throws IOException {
 		//String tmpdir = System.getProperty("java.io.tmpdir");
-		
-		//Path tempFile = Files.createTempFile("dubhe", null);
 		File tempFile = File.createTempFile("dubhe", null);
 		tempFile.deleteOnExit();
-				
+
 		String str = "Hello Dubhe";
 		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile.getAbsolutePath()));
 		writer.write(str);
 		writer.close();
-		
+
 		assertEquals(str, readFile(tempFile));
 	}
+
+	@Test
+	public void checkSum()
+			throws IOException {
+
+		File tempFile = File.createTempFile("dubhe", null);
+		tempFile.deleteOnExit();
+
+		String str = "Hello Dubhe CheckSum";
+		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile.getAbsolutePath()));
+		writer.write(str);
+		writer.close();
+
+		InputStream targetStream = new FileInputStream(tempFile);
+		
+		assertEquals(3467181004L, getChecksumCRC32(targetStream, 1024));
+	}
+
 }
