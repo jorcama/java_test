214e0981afaa766ee125b42d4f9b7aa33aa48c97
jorgec <jorgec@meta4.com>
2022-06-08 11:47:26 +0200

    Update OkHttp rest tests

--- src/test/java/com/dubhe/tests/http/OkHttp.java
+++ src/test/java/com/dubhe/tests/http/OkHttp.java
@@ -4,22 +4,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OkHttp {
 
 	@Test
-	public void shouldGenerateAStringToken()
+	public void shouldCalldatosmadridREST()
 			throws IOException {
 
 		String sURL = "https://datos.madrid.es/egob/catalogo/format/fmt.json";
 		OkHttpClient client = new OkHttpClient().newBuilder().build();
 		Request request = new Request.Builder().url(sURL).method("GET", null).addHeader("Accept", "application/json").build();
+
+		Response response = client.newCall(request).execute();
+
+		System.out.println(response.body().string());
+		assertEquals(HttpURLConnection.HTTP_OK, response.code());
+	}
+
+	@Test
+	public void shouldTextLimitRequestBody()
+			throws IOException {
+
+		final MediaType TEXT_PLAIN = MediaType.parse("text/plain; charset=utf-8");
+
+		String sURL = "https://datos.madrid.es/egob/catalogo/format/fmt.json";
+
+		StringBuffer text = new StringBuffer();
+		for (int i = 0; i < 1000000; i++) {
+			text.append("a");
+		}
+
+		RequestBody body = RequestBody.create(new String(text), TEXT_PLAIN);
+
+		OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(60, TimeUnit.SECONDS).writeTimeout(60, TimeUnit.SECONDS).build();
+
+		Request request = new Request.Builder().url(sURL).method("GET", null).post(body).addHeader("Accept", "text/plain").build();
 
 		Response response = client.newCall(request).execute();
 
