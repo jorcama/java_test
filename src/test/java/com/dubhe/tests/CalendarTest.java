46ba4c7da0ef0d2c9383f910ffc3765e58013f6e
jorgec <jorgec@meta4.com>
2023-03-21 14:17:57 +0100

    Serialize Files

---
+++ src/test/java/com/dubhe/tests/CalendarTest.java
@@ -0,0 +1,40 @@
+package com.dubhe.tests;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashSet;
+
+import org.junit.jupiter.api.Test;
+
+public class CalendarTest
+{
+	@Test
+	public void generateFileName()
+			throws IOException
+	{
+		HashSet<String> fileNames = new HashSet<String>();
+
+		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+
+		String fileName = "payroll-feb.pdf";
+		int iPos = fileName.indexOf(".");
+
+		for (int i = 0; i < 5; i++)
+		{
+			String tempFileName = fileName.substring(0, iPos) + "-" + dateFormat.format(calendar.getTime()) + fileName.substring(iPos, fileName.length());
+			if (i == 0)
+			{
+				assertTrue(fileNames.add(tempFileName));
+			}
+			else
+			{
+				assertFalse(fileNames.add(tempFileName));
+			}
+		}
+	}
+}
--- src/test/java/com/dubhe/tests/SerializationTest.java
+++ src/test/java/com/dubhe/tests/SerializationTest.java
@@ -3,21 +3,32 @@ package com.dubhe.tests;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
 
 import org.junit.jupiter.api.Test;
 
-public class SerializationTest {
+public class SerializationTest
+{
 
 	@Test
 	public void whenSerializingAndDeserializing_ThenObjectIsTheSame()
-			throws IOException, ClassNotFoundException {
+			throws IOException, ClassNotFoundException
+	{
 		Person person = new Person();
 		person.setAge(20);
 		person.setName("Joe");
@@ -41,7 +52,8 @@ public class SerializationTest {
 
 	@Test
 	public void whenCustomSerializingAndDeserializing_ThenObjectIsTheSame()
-			throws IOException, ClassNotFoundException {
+			throws IOException, ClassNotFoundException
+	{
 		Person p = new Person();
 		p.setAge(20);
 		p.setName("Joe");
@@ -53,14 +65,14 @@ public class SerializationTest {
 		e.setPerson(p);
 		e.setAddress(a);
 
-		FileOutputStream fileOutputStream = new FileOutputStream("target/yourfile.txt");
+		FileOutputStream fileOutputStream = new FileOutputStream("target/yourfile.bin");
 		ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 		//Serialize
 		objectOutputStream.writeObject(e);
 		objectOutputStream.flush();
 		objectOutputStream.close();
 
-		FileInputStream fileInputStream = new FileInputStream("target/yourfile.txt");
+		FileInputStream fileInputStream = new FileInputStream("target/yourfile.bin");
 		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 		//Deserialize
 		Employee e2 = (Employee) objectInputStream.readObject();
@@ -68,5 +80,141 @@ public class SerializationTest {
 
 		assertTrue(e2.getPerson().getAge() == e.getPerson().getAge());
 		assertTrue(e2.getAddress().getHouseNumber() == e.getAddress().getHouseNumber());
+	}
+
+	@Test
+	public void serializeAndDeserialezeFilesWithFileWriter()
+			throws IOException, ClassNotFoundException
+	{
+
+		FileOutputStream fileOutputStream = new FileOutputStream("target/yourfile.bin");
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+		for (int i = 0; i < 3; i++)
+		{
+			Path tempFile = Files.createTempFile("~dubhe", ".tmp");
+			File file = new File(tempFile.toString());
+			file.deleteOnExit();
+			String str = "Hello Dubhe" + i;
+			BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile.toString()));
+			writer.write(str);
+			writer.close();
+
+			//Serialize
+			objectOutputStream.writeObject(Files.readAllBytes(tempFile));
+
+		}
+
+		objectOutputStream.flush();
+		objectOutputStream.close();
+
+		FileInputStream fileInputStream = new FileInputStream("target/yourfile.bin");
+
+		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+		for (int i = 0; i < 3; i++)
+		{
+			byte[] bytes = (byte[]) objectInputStream.readObject();
+			Path tempFileResult = Files.createTempFile("~dubhe", ".tmp");
+			File file = new File(tempFileResult.toString());
+			file.deleteOnExit();
+			try (FileOutputStream stream = new FileOutputStream(tempFileResult.toString()))
+			{
+				stream.write(bytes);
+			}
+
+		}
+		objectInputStream.close();
+
+	}
+
+	@Test
+	public void serializeAndDeserialezeFilesWithBuffer()
+			throws IOException, ClassNotFoundException
+	{
+		final int FILE_CHUNCK_SIZE = 16 * 1024;
+		FileOutputStream fileOutputStream = new FileOutputStream("target/yourfile.bin");
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+		int fileNumber = 3;
+		objectOutputStream.writeInt(fileNumber);
+
+		byte[] buffer = new byte[FILE_CHUNCK_SIZE];
+
+		for (int i = 0; i < fileNumber; i++)
+		{
+			//Create temporal file
+			Path tempFile = Files.createTempFile("~dubhe", ".tmp");
+			String str = "Hello Dubhe" + i;
+			BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile.toString()));
+			writer.write(str);
+			writer.close();
+
+			//Serialize
+			File file = new File(tempFile.toString());
+			file.deleteOnExit();
+			objectOutputStream.writeObject(file.getName());
+			//File Size
+			long bytesToWrite = file.length();
+			objectOutputStream.writeLong(bytesToWrite);
+			if (bytesToWrite > 0)
+			{
+				//File content
+				try (FileInputStream fis = new FileInputStream(file))
+				{
+					int count = 0;
+					while ((count = fis.read(buffer)) > -1)
+					{
+						objectOutputStream.write(buffer, 0, count);
+					}
+				}
+			}
+		}
+
+		objectOutputStream.flush();
+		objectOutputStream.close();
+
+		FileInputStream fileInputStream = new FileInputStream("target/yourfile.bin");
+
+		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+		fileNumber = objectInputStream.readInt();
+		if (fileNumber > 0)
+		{
+			for (int i = 0; i < fileNumber; i++)
+			{
+
+				String fileName = (String) objectInputStream.readObject();
+				long bytesToRead = objectInputStream.readLong();
+
+				Path tempFileResult = Files.createTempFile("~dubhedos", ".tmp");
+				File file = new File(tempFileResult.toString());
+				file.deleteOnExit();
+				if (bytesToRead > 0)
+				{
+					try (FileOutputStream fos = new FileOutputStream(tempFileResult.toString()))
+					{
+						while (bytesToRead > 0)
+						{
+
+							int length = buffer.length;
+							if (bytesToRead < length)
+							{
+								length = (int) bytesToRead;
+							}
+							int count = objectInputStream.read(buffer, 0, length);
+							if (count < 0)
+							{
+								throw new EOFException();
+							}
+							bytesToRead -= count;
+
+							fos.write(buffer, 0, count);
+						}
+					}
+				}
+			}
+		}
+		objectInputStream.close();
+
 	}
 }
